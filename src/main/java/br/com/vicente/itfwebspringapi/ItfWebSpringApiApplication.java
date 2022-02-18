dfa435318519d040d4b0b36c04d849290974524d
Vicente Leonardo <vicente.lamberti@hotmail.com>
2022-02-18 13:39:25 -0300

    Criação do projeto

---
+++ src/main/java/br/com/vicente/itfwebspringapi/ItfWebSpringApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.vicente.itfwebspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItfWebSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItfWebSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vicente/itfwebspringapi/ItfWebSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vicente.itfwebspringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItfWebSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
